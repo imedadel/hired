b990d8b8e1c9612997077e292e9171111da5cef2
Imed Adel <geekemad@gmail.com>
2018-11-28 22:57:53 +0100

    Add navigation and Improve design

--- client/src/jobs/Jobs.js
+++ client/src/jobs/Jobs.js
@@ -1,9 +1,122 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Layout, Icon, Card, Avatar, Col, Row} from 'antd';
+import "antd/dist/antd.css";
+import "./Jobs.css";
 
-const Jobs = () => (
-    <main>
-        hello!
-    </main>
-);
+const {Content} = Layout;
+const {Meta} = Card;
+
+class Jobs extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Content style={{margin: '24px 16px', padding: 24, minHeight: 280}}>
+                <Row gutter={20} justify={'space-around'} type={'flex'} align={'middle'}>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+
+                                title="Card title"
+                                description="This is the d escription"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <Row gutter={20} justify={'space-around'} type={'flex'} align={'middle'}>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            </Content>
+        );
+    }
+}
+
+
+// const Jobs = () => (
+//     <main>
+//         hello!
+//     </main>
+// );
 
 export default Jobs;
--- client/src/main/Main.js
+++ client/src/main/Main.js
@@ -1,22 +1,94 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Menu, Icon} from 'antd';
+import "antd/dist/antd.css";
 import Home from '../home/Home';
 import Candids from '../candids/Candids';
 import Jobs from '../jobs/Jobs';
 import Explore from '../explore/Explore';
 import Stats from '../stats/Stats';
 
+const {Header, Sider, Content} = Layout;
 
-const Main = () => (
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: true,
+        };
+    }
 
-    <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/candids' component={Candids}/>
-        <Route path='/jobs' component={Jobs}/>
-        <Route path='/explore' component={Explore}/>
-        <Route path='/stats' component={Stats}/>
-    </Switch>
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
 
-);
+
+    render() {
+        return (
+            <Layout className="jobs-layout">
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className="logo">
+                        Dashboard
+                    </div>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Link to="/">
+                            <Icon type="home"/>
+                            <span>Home</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to="/candids">
+                            <Icon type="team"/>
+                            <span>Candidates</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Link to="/jobs">
+                            <Icon type="folder"/>
+                            <span>Jobs</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            <Link to="/stats">
+                                <Icon type="line-chart"/>
+                                <span>Statistics</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="5">
+                            <Link to="/explore">
+                                <Icon type="github"/>
+                                <span>Explore</span>
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{background: '#fff', padding: 0}}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                        <Switch>
+                            <Route exact path='/' component={Home}/>
+                            <Route path='/candids' component={Candids}/>
+                            <Route path='/jobs' component={Jobs}/>
+                            <Route path='/explore' component={Explore}/>
+                            <Route path='/stats' component={Stats}/>
+                        </Switch>
+
+                </Layout>
+            </Layout>
+        );
+    }
+}
 
 export default Main;
