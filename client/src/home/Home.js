e68b27e48dac2fdea1c0595389ad7860121ff727
Imed Adel <geekemad@gmail.com>
2018-11-28 23:22:43 +0100

    Add homepage

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,9 +1,121 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Layout, Icon, Card, Avatar, Col, Row} from 'antd';
+import "antd/dist/antd.css";
+import "./Home.css";
 
-const Home = () => (
-    <main>
-        hello!
-    </main>
-);
+const {Content} = Layout;
+const {Meta} = Card;
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Content style={{margin: '24px 16px', padding: 24, minHeight: 280}}>
+                <Row gutter={20} justify={'space-around'} type={'flex'} align={'middle'}>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            hoverable={true}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+
+                                title="Card title"
+                                description="This is the d escription"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="./undraw_programming_2svr.png"/>}
+                            actions={[<Icon type="edit"/>, <Icon type="delete"/>]}
+                        >
+                            <Meta
+                                title="Full Stack Developer"
+                                description="React, Express, Node, MongoDB"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <Row gutter={20} justify={'space-around'} type={'flex'} align={'middle'}>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card
+                            style={{width: 300}}
+                            cover={<img alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>}
+                            actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                        >
+                            <Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title="Card title"
+                                description="This is the description"
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            </Content>
+        );
+    }
+}
+
+
+// const Jobs = () => (
+//     <main>
+//         hello!
+//     </main>
+// );
 
 export default Home;
